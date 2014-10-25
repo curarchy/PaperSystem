0bd76a8ca4c7173091a8c3ca3a7fd36e00aa2631
chenhaoyin <chenhaoyin@baidu.com>
2014-10-26 00:26:40 +0800

    test excel

---
+++ Service/ExcelHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+namespace PaperSystem.Service
+{
+    public class ExcelHelper
+    {
+        public static DataSet ExcelToDataset(string Path)
+        {
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Path + ";" + "Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+            strExcel = "select * from [sheet1$]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+            return ds;
+        }
+    }
+}
